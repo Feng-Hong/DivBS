a7a4372a75db85c4d7a2dd09d8fc11b9dee9f4e6
Feng-Hong <59736423+Feng-Hong@users.noreply.github.com>
2024-05-16 00:40:38 +0800

    update

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,2 @@
 from .cifar import CIFAR10, CIFAR100, CIFAR100_LT, CIFAR10_LT, CIFAR100_half, CIFAR10_half
-from .cifar_noisy import CIFAR10N, CIFAR100N
 from .tinyimagenet import TinyImageNet
-from .clothing import Clothing1M
